027f2a7acba8bca6243465cc425843b621618914
Evgeni Atanasov <evgeni.atanasov@Softwaregroup-bg.com>
2017-01-16 17:05:22 +0200

    Fixes if output folder is not present

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-"use strict";
-
-var generate = require( './lib/util' );
-
-var regex = /GetTranslation\(\s*['|\"](\S*)['|\"]\s*\)/g;
-var filesPath = "D:\\Projects\\sample\\pages\\client\\transfer\\a2c";
-var translationFilePath = "D:\\Projectss\\sample\\utNetTranslations.json";
-
-generate( regex, filesPath, translationFilePath );
+module.exports = require( './lib' );
---
+++ sample/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var generate = require( '../lib/' );
+
+var regex = /GetTranslation\(\s*['|\"](\S*)['|\"]\s*\)/g;
+var filesPath = "D:\\Projects\\sample\\pages\\client\\transfer\\a2c";
+var translationFilePath = "D:\\Projectss\\sample\\utNetTranslations.json";
+
+generate( regex, filesPath, translationFilePath );
