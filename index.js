89f68ef309969f5ea9984f0cea7e7db2dd8d023e
Evgeni Atanasov <evgeni.atanasov@Softwaregroup-bg.com>
2017-01-16 16:51:00 +0200

    Change sample code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 var generate = require( './lib/util' );
 
 var regex = /GetTranslation\(\s*['|\"](\S*)['|\"]\s*\)/g;
-var filesPath = "D:\\Projects\\UTNet4_MC\\utNetPresentationLayer\\utNetAgentApplication\\pages\\client\\transfer\\a2c";
-var translationFilePath = "D:\\Projects\\UTNet4_MC\\utNetDataLayer\\utNetData\\system\\MicroCredAgents\\utNetTranslations.json";
+var filesPath = "D:\\Projects\\sample\\pages\\client\\transfer\\a2c";
+var translationFilePath = "D:\\Projectss\\sample\\utNetTranslations.json";
 
 generate( regex, filesPath, translationFilePath );
